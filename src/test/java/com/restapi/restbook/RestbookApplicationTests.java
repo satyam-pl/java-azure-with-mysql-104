31b63dae5b334aca2ff3508138ff834f7d2967bb
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-10-04 12:31:19 +0530

    Add files via upload

---
+++ src/test/java/com/restapi/restbook/RestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.restbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
